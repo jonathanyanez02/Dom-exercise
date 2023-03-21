f1844fb998c301e7f3215b971027d21b6b8d1b06
Jonathan Yanez <jonathanyanez02@gmail.com>
2023-03-20 21:34:27 -0400

    added files

--- script.js
+++ script.js
@@ -1,22 +1,8 @@
-const container = document.querySelector('#container');
-// selects the #container div (don't worry about the syntax, we'll get there)
+const grid = document.getElementById('grid');
 
-
-
-const controls = document.querySelector('.controls');   
-// selects the .controls div
-
-
-const div = document.createElement('div');                     
-// creates a new div referenced in the variable 'div'
-
-div.textContent = 'Hello World!';
-
-div.innerHTML = '<span>Hello World!</span>';   
-
-
-const content = document.createElement('div');
-content.classList.add('content');
-content.textContent = 'THE DOM IS ADDING ME';
-
-container.appendChild(content);
+// Create 16 x 16 grid cells
+for (let i = 0; i < 16 * 16; i++) {
+  const cell = document.createElement('div');
+  cell.classList.add('cell');
+  grid.appendChild(cell);
+}
