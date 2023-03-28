a103dbed983a96ad9992db254f0b8e3ff97e67b5
Jonathan Yanez <jonathanyanez02@gmail.com>
2023-03-27 20:10:44 -0400

    added button

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 const container = document.querySelector('.container');
 
-for (let i = 0; i < 256; i++) {
+for (let i = 0; i <=254; i++) {
   const square = document.createElement('div');
   square.classList.add('square');
   container.appendChild(square);
+}
+
+
+document.querySelector('button').addEventListener('click', function() {
+  let userAdjective = prompt("Please provide a number");
+  alert (userAdjective);
+});
+
+function paintGrid(elem, color){    
+  if(elem.buttons == 1){
+      if(elem.target.classList == 'square'){
+          let square = elem.target;    
+          square.style.backgroundColor = color;
+      }  
+  }else{
+      //Exit condition if mouse is not clicked.
+      return;
+  }
 }
